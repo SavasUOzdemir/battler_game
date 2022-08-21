ae47f5511432e52b32aea83d1612bef3a7b1e06e
SavasUOzdemir <savas.ozdemir@ozu.edu.tr>
2022-08-21 19:47:37 +0300

    assets

---
+++ auto battler/Assets/Scripts/Attributes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attributes : MonoBehaviour
+{
+    public float health = 100f;
+    public float speed = 5f;
+    public float morale = 100f;
+    public float endurance = 100f;
+    public float attackRange = 5f;
+}
