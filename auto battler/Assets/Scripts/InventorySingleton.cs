6bc640bb0477411e5bbdabc4a8873f7f66690ae9
SavasUOzdemir <savas.ozdemir@ozu.edu.tr>
2022-09-29 22:55:12 +0300

    add squad items dictionary, fix bugs in inventory singleton object

--- auto battler/Assets/Scripts/InventorySingleton.cs
+++ auto battler/Assets/Scripts/InventorySingleton.cs
@@ -12,56 +12,99 @@ public class InventorySingleton : MonoBehaviour
     [SerializeField] Squad[] squads = new Squad[10];
 
     [SerializeField] private int currentGold = 0;
-    List<Item> armoryItems;
-    List<Item> squadItems;
-    List<Item> shopItems;
+    [SerializeField] List<Item> armoryItems = new();
+    [SerializeField] List<Item> squadItems = new();
+    [SerializeField] List<Item> shopItems = new();
+    Dictionary<Squad, Item[]> itemsDictionary = new();
 
     public int CurrentGold
-    { 
-        get 
-        { 
-            return currentGold; 
+    {
+        get
+        {
+            return currentGold;
         }
         set
         {
             currentGold = value;
-            goldUI.gameObject.SendMessage("UpdateGoldOnScreen",SendMessageOptions.RequireReceiver);
+            goldUI.gameObject.SendMessage("UpdateGoldOnScreen", SendMessageOptions.RequireReceiver);
         }
     }
-    //public void UpdateItem()
+
+    //void TestDictionary()
     //{
     //    foreach (Squad squad in squads)
-    //    {
-    //        for (int i = 0; i < squad.heldItems.Length; i++)
-    //        {
-    //            if (squad.heldItems[i]!= null)
-    //            {
-    //                InventorySingleton.instance.squadItems.Add(squad.heldItems[i]);
-    //            }
-    //        }
-    //    }
-
-    //    for (int i = 0; i < armory.transform.childCount; i++)
-    //    {
-    //        if (armory.transform.GetChild(i).GetComponent<ItemButton>() != null)
-    //        {
-    //            Debug.Log(armory.transform.GetChild(i).GetComponent<ItemButton>().Item);
-    //            InventorySingleton.instance.armoryItems.Add(armory.transform.GetChild(i).GetComponent<ItemButton>().GetComponent<Item>());
-    //            continue;
-    //        }
-    //        else break;
-    //    }
-
-    //    for (int i = 0; i < shop.transform.childCount; i++)
-    //    {
-    //        if (shop.transform.GetChild(i).GetComponent<ItemButton>().Item != null)
-    //        {
-    //            InventorySingleton.instance.shopItems.Add(shop.transform.GetChild(i).GetComponent<ItemButton>().Item);
-    //            continue;
-    //        }
-    //        else break;
-    //    }
+    //        for (int i = 0; i < 4; i++)
+    //            Debug.Log(itemsDictionary[squad][i]);
     //}
+
+    void UpdateDictionary()
+    {
+        itemsDictionary.Clear();
+        foreach (Squad squad in squads)
+        {
+            itemsDictionary.Add(squad, squad.heldItems);
+        }
+    }
+
+    void UpdateTotalSquadItemList()
+    {
+        squadItems.Clear();
+        foreach (Squad squad in squads)
+        {
+            for (int i = 0; i < squad.heldItems.Length; i++)
+            {
+                if (squad.heldItems[i] != null)
+                {
+                    squadItems.Add(squad.heldItems[i]);
+                }
+            }
+        }
+    }
+
+    void UpdateArmoryItemList()
+    {
+        armoryItems.Clear();
+        for (int i = 0; i < armory.transform.childCount; i++)
+        {
+            if (armory.transform.GetChild(i).GetComponent<ItemButton>() != null)
+            {
+                if (armory.transform.GetChild(i).GetComponent<ItemButton>().Item != null && armoryItems != null)
+                {
+                    armoryItems.Add(armory.transform.GetChild(i).GetComponent<ItemButton>().Item);
+                }
+                continue;
+            }
+            else break;
+        }
+    }
+
+    void UpdateShopItemList()
+    {
+        shopItems.Clear();
+        for (int i = 0; i < shop.transform.childCount; i++)
+        {
+            if (shop.transform.GetChild(i).GetComponent<ItemButton>() != null)
+            {
+
+                if (shop.transform.GetChild(i).GetComponent<ItemButton>().Item != null)
+                {
+                    shopItems.Add(shop.transform.GetChild(i).GetComponent<ItemButton>().Item);
+                    continue;
+                }
+            }
+            else break;
+        }
+    }
+
+
+    public void UpdateItem()
+    {
+        UpdateDictionary();
+        UpdateTotalSquadItemList();
+        UpdateArmoryItemList();
+        UpdateShopItemList();
+        //TestDictionary();
+    }
 
     private void Awake()
     {
