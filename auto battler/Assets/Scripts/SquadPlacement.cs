37d26140071bb2a2f3569e66f144e1c7961350a5
SavasUOzdemir <savas.ozdemir@ozu.edu.tr>
2022-10-04 19:13:06 +0300

    add unfinished warscreen implementations

--- auto battler/Assets/Scripts/SquadPlacement.cs
+++ auto battler/Assets/Scripts/SquadPlacement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DapperDino.TooltipUI;
 
 
@@ -8,7 +9,7 @@ public class SquadPlacement : MonoBehaviour
 {
     [SerializeField] Squad[] squads = new Squad[10];
     bool defaultPlacement = true;
-    
+
     private void OnEnable()
     {
         if (defaultPlacement)
@@ -23,6 +24,19 @@ public class SquadPlacement : MonoBehaviour
         else if (!defaultPlacement)
         {
 
+        }
+        UpdateThumbnails();
+    }
+    private void UpdateThumbnails()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).GetComponent<HoldSquad>().Squad != null)
+            {
+                transform.GetChild(i).GetComponent<Image>().sprite = transform.GetChild(i).GetComponent<HoldSquad>().Squad.Sprite;
+            }
+            else
+                transform.GetChild(i).GetComponent<Image>().sprite = null;
         }
     }
 }
