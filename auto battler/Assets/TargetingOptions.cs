266f9413e81578c4ad6a5cdb4cc73c5eabae4cfb
SavasUOzdemir <savas.ozdemir@ozu.edu.tr>
2022-10-11 03:47:22 +0300

    develop formation and targeting options (partially)

--- auto battler/Assets/TargetingOptions.cs
+++ auto battler/Assets/TargetingOptions.cs
@@ -13,24 +13,41 @@ public class TargetingOptions : MonoBehaviour
     [SerializeField] GameObject highestHP;
     [SerializeField] GameObject protective;
     GameObject[] buttons;
+    string formation_ = null;
     List<string> targetOpts;
+
+    public string Formation
+    {
+        get
+        {
+            return formation_;
+        }
+        set
+        {
+            formation_ = value;
+            UpdateTargetingOptions(formation_);
+        }
+    }
+    
     private void Awake()
     {
         buttons = new GameObject[8] {closestSquad, lowestHealth, lowestMorale, lowestEndurance, rangedSquads, flyingSquads, highestHP, protective};
-        DisableAll();
+        DisableAllButtons();
     }
 
 
-    void DisableAll()
+    void DisableAllButtons()
     {
         foreach (GameObject button in buttons)
-        {
-            button.SetActive(false);
-        }
+            button.SetActive(false);        
     }
 
     void UpdateTargetingOptions(string formation)
     {
         targetOpts = CompanyFormations.GetTargetingOptions(formation, 0);
+        foreach (var opt in targetOpts)
+        {
+            Debug.Log(opt);
+        }
     }
 }
