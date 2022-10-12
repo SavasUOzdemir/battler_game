bb7b16c7a2af7acf5c50f0e30a5865c5c34e53f8
SavasUOzdemir <savas.ozdemir@ozu.edu.tr>
2022-10-12 03:48:42 +0300

    implement targeting buttons to change squad targeting mode

--- auto battler/Assets/TargetingModeHook.cs
+++ auto battler/Assets/TargetingModeHook.cs
@@ -5,9 +5,31 @@ using UnityEngine;
 public class TargetingModeHook : MonoBehaviour
 {
     [SerializeField] string targetingModeName = null;
+    [SerializeField] GameObject targetingPanel;
+    [SerializeField] GameObject formationsPanel;
+    [SerializeField] Company company;
+    bool primarySet = false;
 
+    private void OnEnable()
+    {
+        company = formationsPanel.GetComponent<FormationOptions>().Company;
+    }
     public string TargetingModeName
     {
         get { return targetingModeName; }
+    }
+
+    public void OnButtonPress()
+    {
+        if (!primarySet)
+        {
+            company.GetComponent<Company>().ChangePrimaryTargetingMode(targetingModeName);
+            primarySet = !primarySet;
+        }
+        else
+        { 
+            company.GetComponent<Company>().ChangeSecondaryTargetingMode(targetingModeName); 
+            primarySet = !primarySet;            
+        }
     }
 }
